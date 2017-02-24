eda8a91d3c85c006256698c07fcbf24441da99d1
Sven Cremer <sven.cremer@mavs.uta.edu>
2017-02-24 17:37:41 -0600

    Neuroadptive controller with prescribed error dynamics.

--- neuroAdaptiveController.m
+++ neuroAdaptiveController.m
@@ -1,6 +1,7 @@
 function [fc,fc_exp] = neuroAdaptiveController(q, qd, x, xd, x_m, xd_m, xdd_m, dt)
 
-global W V
+global W V                  % NN weights
+global gamma lambda fl      % Prescribed Error dynamics
 
 input  = size(V,1);
 hidden = size(V,2);
@@ -8,17 +9,31 @@ output = size(W,2);
 
 % NN controller parameters
 Kv     = 10*eye(output)  ;
-lam    = 20*eye(output)  ;
+%lambda = 20*eye(output)  ;
 F      = 100*eye(hidden) ;
 G      = 100*eye(input)   ;
 kappa  = 0.001             ;
 Kz     = 0.05            ;
 Zb     = 100              ;
 
+% Prescribed Error dynamics
+fh = [0.0;0.0];                    % TODO measure
+Kd = 20.0*eye(output);
+Dd = 10.0*eye(output);
+gamma = Dd - lambda;
+lambda_dot = Kd - gamma*lambda;
+lambda = lambda + lambda_dot*dt;    % New lambda
+% if(max(lambda(:)) > 25)
+%     lambda = 25*eye(output);
+% end
+% gamma  = Dd - lambda;               % New gamma
+fl_dot = fh - gamma*fl;
+fl = fl + fl_dot*dt;                % New fl
+
 % Tracking errors
 e  = x_m  - x   ;
 ed = xd_m - xd  ;
-r  = ed  + lam*e ;  % sliding mode error
+r  = ed  + lambda*e - fl ;          % Sliding mode error
 
 % Robustifying term
 Z = [ W, zeros(size(W, 1), size(V, 2));
@@ -37,7 +52,7 @@ fc = Kv*r + 1.0*(f_hat - v);    % TODO flag for NN off
 
 % Expected Control force
 [Mx,Cx,Gx] = robotDynamicsCartesian(q, qd);
-f_act = Mx*( xdd_m + lam*ed ) + Cx*( xd_m + lam*e ) + Gx;
+f_act = Mx*( xdd_m + lambda*ed ) + Cx*( xd_m + lambda*e ) + Gx;
 fc_exp = Kv*r + f_act - v;
 
 % Parameter update
