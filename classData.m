78b708dc951fd94554c5120981cfa497b3a891f5
Sven Cremer <sven.cremer@mavs.uta.edu>
2017-03-01 14:45:42 -0600

    Function for plotting data variables versus time.

--- classData.m
+++ classData.m
@@ -4,7 +4,8 @@ classdef classData
     properties
         % Public properties; underscore, i.e. x_, indicates a function of time
         
-        t;  % Time vector
+        t;      % Time vector
+        idx;    % Time index
         
         % NA weights and controller parameters
         normW_;
@@ -32,11 +33,11 @@ classdef classData
         q_;    % Joint position
         qd_;   % Joint velocity
         
-        ode_t;      % ODE simulation time vector
+        ode_t;     % ODE simulation time vector
         ode_x_;    % ODE simulation states
         
         % Human state
-        f_h_;    % Human force
+        fh_;     % Human force
         x_h_;    % Cartesian pose
         xd_h_;   % Cartesian velocity
         
@@ -64,14 +65,16 @@ classdef classData
                 data.nJoints = nJoints;
             end
             
+            data.idx = 1;
+            
             % Preallocate memory
             data.t       = zeros(N,1);
             
             data.normW_  = zeros(N,1);
             data.normV_  = zeros(N,1);
             
-            data.lambda_ = zeros(N,nOut*2);
-            data.gamma_  = zeros(N,nOut*2);
+            data.lambda_ = zeros(N,nOut);   % Assume diagonal
+            data.gamma_  = zeros(N,nOut);
             data.fl_     = zeros(N,nOut);
             
             %data.f_hat_  = zeros(N,numOut);
@@ -134,6 +137,87 @@ classdef classData
             end
             
         end
+        
+        function plotVariable(data, str, new_fig, varargin)
+            % Plots class variable defined in string str
+            
+            if( sum( strcmp(str,fieldnames(data)) ) == 0 )
+                fprintf('Error: Variable ''%s'' not found!\n',str)
+                return
+            end
+            
+            % Check if a new figure should be drawn
+            if ~exist('new_fig','var') || isempty(new_fig) || new_fig==1
+                figure;
+                new_fig = 1;
+            else
+                hold on;
+                % Extract numbers in ylabels for selecting correct subplot
+                h_ax = get(gcf,'children');
+                h_la = get(h_ax,'ylabel');
+                J = length(h_la);
+                lab = zeros(J,1); % Mapping from subplot index to label number
+                for j=1:J
+                    s = get(h_la{j},'string');
+                    A = regexp(s,'\d*','Match');
+                    if( length(A)==1 )
+                        lab(j) = str2num(A{1});
+                    end
+                end
+            end
+            
+            if ~exist('varargin','var') || isempty(varargin)
+                varargin = {'-b'};
+            end
+            
+            k = data.idx-1;
+            
+            y = eval(['data.',str,'(1:k,:)']);
+            t = data.t(1:k);
+            
+            N = size(y,2);
+            
+            if(N == 1)
+                
+                plot(t, y, varargin{:})
+                
+            else
+                
+                set(gcf,'position',[75   675   560   840]);
+                
+                for i=1:N
+                    % Select subplot
+                    if(new_fig)
+                        subplot(N,1,i);
+                    else
+                        idx = 1;
+                        for j=1:length(lab) 
+                            if lab(j) == i
+                                idx = j;
+                            end
+                        end
+                        subplot( h_ax(idx) )
+                    end
+                    grid on; hold on;
+                    
+                    plot(t, y(:,i),varargin{:})
+                    %plot(data.t, data.tau_exp_(:,i),':r')
+                    
+                    xlabel('Time [s]');
+                    ylabel(sprintf('Joint %d [rad]',i))
+                    suptitle(strrep(str,'_','\_'))
+                end
+            end
+            
+        end
+        
+%         function plot(data,varargin)
+%             plot(data.t,varargin{:})
+%         end
+        
+%         function disp(data)
+%             fprintf(data. ...);
+%         end
              
     end
     
