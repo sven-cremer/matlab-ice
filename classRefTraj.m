66afea81aadf010e24c1328c953dc0f75b1d8a61
Sven Cremer <sven.cremer@mavs.uta.edu>
2017-03-19 15:32:03 -0500

    Re-written RefTraj class to store instead of returning trajectories.

--- classRefTraj.m
+++ classRefTraj.m
@@ -5,51 +5,121 @@ classdef classRefTraj
     properties
         % Public properties
         
+        nJ;     % Number of robot joints
         N;      % Number of trajectory points
-        %x0;
-        %q0;
+        dt;     % Time step
+        t;      % Time vector
+        tf;     % End time
+
+        x;      % Cartesian reference trajectory
+        xd;
+        xdd;
+        q;      % Joint reference trajectory
+        qd;
         
-        %t0;
-        %tf;
-        
+        x0;     % Start pose
+        xf;     % Final pose
+        q0;     % Start joint position
+        qf;     % Final joint position
+            
     end
     
     methods     % constructor method
         
-        function o = classRefTraj()
+        function o = classRefTraj(numJoints)
             
-            o.N = 100;
+            o = reset(o);
+            o.nJ = numJoints;
             
+        end
+        
+        function o = reset(o)
+            % Resets all class variables
+            o.N  = 0;
+            o.dt = 0.001;
+            o.t   = [];
+            o.x   = [];
+            o.xd  = [];
+            o.xdd = [];
+            o.q   = [];
+            o.qd  = [];
+            o.x0 = 0; o.xf = 0;
+            o.q0 = 0; o.qf = 0;
+            o.tf = 0;
         end
         
     end
     
     methods     % computational methods
-           
         
-        function [xref, q, qd] = circular(o, robot, x0, radius, N)
-            % Circular reference trajectory
-            % x0 [3x1]
+        function o = updateVariables(o)
+            % Update class variables assuming a new x(t) and q(t) has been
+            % generated
+            if(o.N < 1)
+                disp('Initialize a trajectory first!');
+                return
+            end
             
-            x0 = transl(x0);
+            % Start and final points
+            o.x0 = o.x(1,:);         
+            o.q0 = o.q(1,:);
+            o.xf = o.x(end,:);
+            o.qf = o.q(end,:);
+            o.tf = o.t(end);
             
-            TC = [];
-            for i=1:N
-                TC(:,:,i)= x0*trotz(-pi/2)*troty(2*pi*i/N)*transl(0, 0, radius);
+            % Derivatives
+            o.xd  = [zeros(1,6); diff(o.x) ]./o.dt;
+            o.xdd = [zeros(1,6); diff(o.xd)]./o.dt;
+            
+            if(isempty(o.qd))
+                o.qd  = [zeros(1,o.nJ); diff(o.q)]./o.dt;
+            end
+            
+            m = max(o.xd);
+            fprintf('Max velocities: %.3f[m/s], %.3f[rad/s]\n',max(m(1:3)), max(m(4:6)));
+        end
+            
+        function o = circular(o, robot, x0, radius, dt, tf)
+            % Circular reference trajectory in yz plane
+            % x0 [3x1] starting position
+            
+            o.nJ = robot.n;
+            
+            % Time vector
+            o.dt = dt;
+            o.t  = 0:o.dt:tf;
+            o.N  = length(o.t);
+            
+            % Pose vector
+            o.x0 = transl(x0);          
+            TC = zeros(4,4,o.N);
+            for i=1:o.N
+                TC(:,:,i)= o.x0*trotz(-pi/2)*troty(2*pi*i/o.N)*transl(0, 0, radius);
             end
             
             xC = transl(TC);
             rC = tr2rpy(TC);
-            %xref = [xC unwrap(rC)];
-            xref = [xC repmat(rC(1,:),[N,1])];
+            %rC = unwrap(tr2rpy(TC));
+            rC = repmat(rC(1,:),[o.N,1]);         
+            o.x   = [xC rC];
             
-            q  = robot.ikine6s(TC, 'run');      % TODO move? takes a lot of time ...
-            qd = [zeros(1,robot.n); diff(q)];
+            % Joint Vector
+            o.q  = robot.ikine6s(TC, 'run');  % TODO move? takes a lot of time ...
+            
+            o = updateVariables(o);
             
         end
         
-        function [xref, q, qd] = straightJtraj(o, robot, x0, r0, x1, r1, N)
+        function o = straightJtraj(o, robot, x0, r0, x1, r1, dt, tf)
+            % Trajectory from x0 to x1 using jtraj function
+            o.nJ = robot.n;
             
+             % Time vector
+            o.dt = dt;
+            o.t  = 0:o.dt:tf;
+            o.N  = length(o.t);
+ 
+            % Joint vector
             T0 = transl(x0)*rpy2tr(r0);
             T1 = transl(x1)*rpy2tr(r1);
             
@@ -58,58 +128,78 @@ classdef classRefTraj
             q0 = robot.ikine6s(T0);
             q1 = robot.ikine6s(T1);
             
-            [q, qd] = jtraj(q0, q1, N ); 
+            [o.q, o.qd] = jtraj(q0, q1, o.N ); 
             
-            TC = robot.fkine(q);
-          
+            % Pose vector
+            TC = robot.fkine(o.q);
             xC = transl(TC);
             rC = unwrap( tr2rpy(TC) );
-            xref = [xC rC];     % TODO check for discontinuity in rC
+            o.x = [xC rC];     % TODO check for discontinuity in rC
+            
+            o = updateVariables(o);
         end
         
-        function [xref, q, qd] = straightCtraj(o, robot, x0, r0, x1, r1, N)
+        function o = straightCtraj(o, robot, x0, r0, x1, r1, dt, tf)
+            % Trajectory from x0 to x1 using ctraj function
+            o.nJ = robot.n;
             
+            % Time vector
+            o.dt = dt;
+            o.t  = 0:o.dt:tf;
+            o.N  = length(o.t);
+            
+            % Pose vector
             T0 = transl(x0)*rpy2tr(r0);
             T1 = transl(x1)*rpy2tr(r1);
             
-            TC = ctraj(T0, T1, N );
-          
+            TC = ctraj(T0, T1, o.N );         
             xC = transl(TC);
             rC = tr2rpy(TC);
-            xref = [xC rC];
-            
-            q  = robot.ikine6s(TC);
-            qd = [zeros(1,robot.n); diff(q)];
+            o.x = [xC rC];
+
+            % Joint vector
+            o.q = robot.ikine6s(TC);
+
+            o = updateVariables(o);
         end
         
-        function [xt_, q_, qd_] = holdEndpoints(o,xref,q,qd,t,dtStart,dtStop)    % TODO pass back q
+        function o = holdEndpoints(o,dtStart,dtStop)
+            % Hold endpoints
+            if(o.N < 1)
+                disp('Initialize a trajectory first!');
+                return
+            end
             
-            %t    = xt(:,1);
-            %xref = xt(:,2:7);
+            qz = zeros(1,o.nJ);
+            xz = zeros(1,6);
             
-            N = length(t);
-            dt = median(diff(t));
+            % Data points
+            n0 = round(dtStart/o.dt);
+            nf = round(dtStop/o.dt);
             
-            % Start
-            n0 = round(dtStart/dt);
-            x_0 = repmat(xref(1,:),[n0 1]);
-            q_0  = repmat(q(1,:),[n0 1]);
-            qd_0 = repmat(qd(1,:),[n0 1]);
+            % Start          
+            x_0  = repmat(o.x0, [n0 1]);
+            xd_0 = repmat(xz,   [n0 1]);          
+            q_0  = repmat(o.q0, [n0 1]);
+            qd_0 = repmat(qz,   [n0 1]);
             
             % Stop
-            nf = round(dtStop/dt);
-            x_f = repmat(xref(end,:),[nf 1]);
-            q_f  = repmat(q(end,:),[nf 1]);
-            qd_f = repmat(qd(end,:),[nf 1]);
+            x_f  = repmat(o.xf, [nf 1]);
+            xd_f = repmat(xz,   [nf 1]);         
+            q_f  = repmat(o.qf, [nf 1]);
+            qd_f = repmat(qz,   [nf 1]);
             
-            % Create trajectories
-            xref_new = [x_0; xref; x_f];
-            t_new = linspace(0, t(end)-t(1)+dtStart+dtStop, N+n0+nf)';
+            % Extend trajectories
+            o.x   = [x_0;  o.x;   x_f];
+            o.xd  = [xd_0; o.xd;  xd_f];
+            o.xdd = [xd_0; o.xdd; xd_f];
             
-            xt_ = [t_new xref_new];
+            o.q   = [q_0;  o.q;   q_f];
+            o.qd  = [qd_0; o.qd;  qd_f];
             
-            q_ = [q_0; q; q_f];
-            qd_ = [qd_0; qd; qd_f];
+            o.t = linspace(0, o.t(end)-o.t(1)+dtStart+dtStop, o.N+n0+nf)';
+            o.N = length(o.t);
+            o.tf = o.t(end);
             
         end
         
@@ -117,7 +207,7 @@ classdef classRefTraj
     
     methods     % for visualization
         
-        function plotTraj(o, xt)
+        function plotTraj(o)
             % Visualize robot trajectory
             
             p = [75   675   560   840];
@@ -126,13 +216,13 @@ classdef classRefTraj
             set(h, 'position', p);
             
             subplot(2,1,1)
-            plot(xt(:,1),xt(:,2:4))
+            plot(o.t,o.x(:,1:3))
             xlabel('Time [s]'); ylabel('Position [m]');
             legend('x','y','z');
             grid on;
             
             subplot(2,1,2)
-            plot(xt(:,1),xt(:,5:7))
+            plot(o.t,o.x(:,4:6))
             xlabel('Time [s]'); ylabel('Rotation [rad]');
             legend('roll','pitch','yaw');
             grid on;
